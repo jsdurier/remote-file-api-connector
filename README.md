e9749a6a79ea0dc2a0dc3b006ebf41301f4eaaf0
jsdurier <jsdurier@gmail.com>
2022-01-22 21:25:15 +0100

    upgrade angular 13.1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# @jsd/remote-file-api-connector
+# @jsdurier/remote-file-api-connector
 
 Connect an angular app to a local instance server "remote-file-api"
 The web app can write/read files on the system through the "remote-file-api" local server.
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RemoteFileApiConnectorModule } from '@jsd/remote-file-api-connector';
+import { RemoteFileApiConnectorModule } from '@jsdurier/remote-file-api-connector';
 
 import { AppComponent } from './app.component';
 
